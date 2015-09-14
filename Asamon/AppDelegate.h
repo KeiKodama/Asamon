354940082a41690262c57554a143207387e10f10
KeiKodama <eastplatform1b@EastPlatform1B-no-MacBook-Air.local>
2015-09-14 14:34:38 +0900

    Initial Commit

---
+++ Asamon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Asamon
+//
+//  Created by EastPlatform1B on 2015/09/14.
+//  Copyright (c) 2015年 pfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Asamon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Asamon
+//
+//  Created by EastPlatform1B on 2015/09/14.
+//  Copyright (c) 2015年 pfd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
